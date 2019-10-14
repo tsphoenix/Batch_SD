fad9df8156e7d2fecf82a6483ef667de17ac95b5
tsphoenix <46634852+tsphoenix@users.noreply.github.com>
2019-10-14 23:56:37 +0200

    Create open_folders_for_cleanup_User_Level.bat

---
+++ open_folders_for_cleanup_User_Level.bat
@@ -0,0 +1,4 @@
+explorer "%userprofile%\AppData\Local\Temp\"
+explorer "%userprofile%\AppData\Local\Microsoft\Windows\INetCookies\"
+explorer "%userprofile%\AppData\Local\Microsoft\Windows\INetCache\"
+explorer C:\appsensevirtual
