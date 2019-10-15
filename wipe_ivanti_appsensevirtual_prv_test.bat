ab3669d0acd76e3c0628caa5296b4cab1114e947
tsphoenix <46634852+tsphoenix@users.noreply.github.com>
2019-10-15 07:56:29 +0200

    Update wipe_ivanti_appsensevirtual_prv_test.bat

--- wipe_ivanti_appsensevirtual_prv_test.bat
+++ wipe_ivanti_appsensevirtual_prv_test.bat
@@ -1,2 +1,11 @@
+@echo off
+title wipe_ivanti_appsensevirtual_prv_test
+
 del /f /s /q "C:\appsensevirtual\"
-rd /s /q "C:\appsensevirtual\")
+rd /s /q "C:\appsensevirtual\"
+
+cls
+echo.
+echo C:\appsensevirtual\ cleared
+echo.
+pause
