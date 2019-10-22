d2bfef4e68dba9d75636854e980fdef027aee2ac
tsphoenix <46634852+tsphoenix@users.noreply.github.com>
2019-10-22 19:00:00 +0200

    reverted to initial code

--- credentialsManagerCleaner.bat
+++ credentialsManagerCleaner.bat
@@ -1,1 +1,1 @@
-For /F "tokens=1,2 delims= " %%G in ('cmdkey /list ^| findstr Target') do  cmdkey /delete %%H
+For /F "tokens=1,2 delims= " %G in ('cmdkey /list ^| findstr Target') do  cmdkey /delete %H
