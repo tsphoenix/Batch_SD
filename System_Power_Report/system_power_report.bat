123a3f21803b96092216b6974741cfc800fcfa06
tsphoenix <46634852+tsphoenix@users.noreply.github.com>
2019-10-22 22:02:38 +0200

    Update and rename system_power_report.bat to system_power_report_prv.bat

--- System_Power_Report/system_power_report.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title System_Power_Report
-
-powercfg /sleepstudy /output "%UserProfile%\Desktop\sleepstudy_report.html"
-
-echo.
-echo sleepstudy_report.html file created on the Desktop
-echo.
-pause
