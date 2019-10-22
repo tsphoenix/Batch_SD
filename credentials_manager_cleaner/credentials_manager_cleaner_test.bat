3744ab38ce506c034be4c89e805ea91b621bf3a6
tsphoenix <46634852+tsphoenix@users.noreply.github.com>
2019-10-22 23:44:35 +0200

    fixed for bat

--- credentials_manager_cleaner/credentials_manager_cleaner_test.bat
+++ credentials_manager_cleaner/credentials_manager_cleaner_test.bat
@@ -1,7 +1,7 @@
 @echo off
 title Credentials Manager Cleaner
 
-For /F "tokens=1,2 delims= " %G in ('cmdkey /list ^| findstr Target') do cmdkey /delete %H
+For /F "tokens=1,2 delims= " %%G in ('cmdkey /list ^| findstr Target') do  cmdkey /delete %%H
 
 echo.
 echo Removed Credentials from Credentials Manager
