e026d8875cb22a1bd12f82076a7348c39eda2bfa
tsphoenix <46634852+tsphoenix@users.noreply.github.com>
2019-10-22 22:07:00 +0200

    Update system_power_report_prv.bat

--- System_Power_Report/system_power_report_prv.bat
+++ System_Power_Report/system_power_report_prv.bat
@@ -1,9 +1,9 @@
 @echo off
 title System_Power_Report_Run_as_administrator
 
-powercfg /sleepstudy /output "%UserProfile%\Desktop\system_power_report.html"
+powercfg /batteryreport /sleepstudy /energy /output "%UserProfile%\Desktop\system_power_report.html"
 
 echo.
-echo system_power_report.html (aka sleepstudy) file created on the Desktop
+echo system_power_report.html  file created on the Desktop
 echo.
 pause
