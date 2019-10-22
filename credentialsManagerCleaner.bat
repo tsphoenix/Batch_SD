3dd77ddad64a50bce06890354a67bb9d08d3eedc
tsphoenix <46634852+tsphoenix@users.noreply.github.com>
2019-10-22 19:01:47 +0200

    added title and exit info

--- credentialsManagerCleaner.bat
+++ credentialsManagerCleaner.bat
@@ -1,1 +1,9 @@
+@echo off
+title Credentials Manager Cleaner
+
 For /F "tokens=1,2 delims= " %G in ('cmdkey /list ^| findstr Target') do  cmdkey /delete %H
+
+echo.
+echo Removed Credentials from Credentials Manager
+echo.
+pause
