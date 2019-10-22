14b70280d3727c04363b1e18e43a1907d7bc69de
tsphoenix <46634852+tsphoenix@users.noreply.github.com>
2019-10-22 07:59:35 +0200

    cleanup

--- Win_10_IE_Fix_User_Level.bat
+++ Win_10_IE_Fix_User_Level.bat
@@ -6,10 +6,6 @@ echo Win_10_IE_Fix_User_Level v1 @tsphoenix
 taskkill /im iexplore.exe
 taskkill /im /f iexplore.exe
 
-if exist "%userprofile%\AppData\Local\Temp\" (
-del /f /s /q "%userprofile%\AppData\Local\Temp\*.*"
-rd /s /q "%userprofile%\AppData\Local\Temp\")
-
 if exist "%userprofile%\AppData\Local\Microsoft\Windows\INetCookies\" (
 del /f /s /q "%userprofile%\AppData\Local\Microsoft\Windows\INetCookies\*.*"
 rd /s /q "%userprofile%\AppData\Local\Microsoft\Windows\INetCookies\")
@@ -18,9 +14,13 @@ if exist "%userprofile%\AppData\Local\Microsoft\Windows\INetCache\" (
 del /f /s /q "%userprofile%\AppData\Local\Microsoft\Windows\INetCache\*.*"
 rd /s /q "%userprofile%\AppData\Local\Microsoft\Windows\INetCache\")
 
+if exist "%userprofile%\AppData\Local\Temp\" (
+del /f /s /q "%userprofile%\AppData\Local\Temp\*.*"
+rd /s /q "%userprofile%\AppData\Local\Temp\")
+
 cls
 echo.
-echo TEMP cleaned, INetCookies cleaned, echo INetCache cleaned
+echo Closed IE, INetCookies cleaned, echo INetCache cleaned, TEMP cleaned
 echo.
 echo Win_10_IE_Fix_User_Level executed without problems
 pause
